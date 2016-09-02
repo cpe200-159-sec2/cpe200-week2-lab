dfdc54e5bbf62394896f7993c51690aa1ed725c0
nohajime <patchara0pt002@gmail.com>
2016-09-02 16:14:23 +0700

    complete

--- lab2_2/src/ArrayStack.java
+++ lab2_2/src/ArrayStack.java
@@ -20,7 +20,8 @@ public class ArrayStack {
 
     public void push(Object o)
     {
-        /* Your code here */
+        top++;
+        storage[top]=o;
     }
 
     public Object pop()
@@ -32,8 +33,9 @@ public class ArrayStack {
 
     public int size()
     {
-        /* Your code here */
-        return 0;
+        if(top==-1) {
+            return 0;
+        }return top;
     }
 
     public static void main(String[] args) {
