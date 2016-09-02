d28cd5f0442340876ae23bfd2108aa6c775b457c
Jeletwilight <koonjj@gmail.com>
2016-09-02 16:34:07 +0700

    LAB 2.1 - 2.2

--- lab2_2/src/ArrayStack.java
+++ lab2_2/src/ArrayStack.java
@@ -20,7 +20,11 @@ public class ArrayStack {
 
     public void push(Object o)
     {
-        /* Your code here */
+        if(top > 4) throw new RuntimeException("Stack overflow");
+        else{
+            top++;
+            storage[top] = o;
+        }
     }
 
     public Object pop()
@@ -32,8 +36,7 @@ public class ArrayStack {
 
     public int size()
     {
-        /* Your code here */
-        return 0;
+        return top+1;
     }
 
     public static void main(String[] args) {
