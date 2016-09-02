49cf16ea9c19bbd83afe5b59fb9e4c438c698b4d
ahar0007 <ahar0007@hotmail.co.th>
2016-09-02 16:17:37 +0700

    580610693 All lab 2

--- lab2_2/src/ArrayStack.java
+++ lab2_2/src/ArrayStack.java
@@ -20,7 +20,10 @@ public class ArrayStack {
 
     public void push(Object o)
     {
-        /* Your code here */
+        if(top==DEFAULT_STORAGE_SIZE-1) throw new RuntimeException("Stack overflow");
+        top++;
+        storage[top]=o;
+
     }
 
     public Object pop()
@@ -32,8 +35,7 @@ public class ArrayStack {
 
     public int size()
     {
-        /* Your code here */
-        return 0;
+        return top+1;
     }
 
     public static void main(String[] args) {
