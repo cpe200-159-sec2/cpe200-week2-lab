30ddfb3bbf13acd9bbb50747276bc4cd0dc34c5d
A550J <nice.dark@hotmail.com>
2016-09-02 17:07:18 +0700

    lab2_2 finished

--- lab2_2/src/ArrayStack.java
+++ lab2_2/src/ArrayStack.java
@@ -20,6 +20,8 @@ public class ArrayStack {
 
     public void push(Object o)
     {
+        top +=1;
+        storage[top]=o;
 
     }
 
@@ -32,8 +34,8 @@ public class ArrayStack {
 
     public int size()
     {
-        /* Your code here */
-        return 0;
+
+        return top+1;
     }
 
     public static void main(String[] args) {
