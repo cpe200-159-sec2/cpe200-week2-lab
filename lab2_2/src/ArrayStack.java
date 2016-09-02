ebf8ae798d5646d7b2ef15d08061bd1a045ff88d
Nachpon <nachpon.petai@gmail.com>
2016-09-02 16:49:49 +0700

    LAB 2.2 fix2

--- lab2_2/src/ArrayStack.java
+++ lab2_2/src/ArrayStack.java
@@ -20,7 +20,7 @@ public class ArrayStack {
 
     public void push(Object o)
      {
-         if(top > storage.length-1){ throw new RuntimeException("Stack underflow");
+         if(top > storage.length-1){ throw new RuntimeException("Stack overflow");
          }else {
              top++;
              storage[top] = o;
