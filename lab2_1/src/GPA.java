f666c4a99f6846e8ad5ca730ef1c072eb5e7e220
focsvpm5 <Ratchaponga@gmail.com>
2016-09-02 15:44:12 +0700

    complete

--- lab2_1/src/GPA.java
+++ lab2_1/src/GPA.java
@@ -2,14 +2,17 @@
  * Implement grading system.
  */
 public class GPA {
+    int sumcredit=0;
+    float sum=0;
     public void addGrade(int credit, float grade) {
-        /* Your code here */
+        sumcredit += credit;
+        sum += credit*grade;
     }
 
 
     public float get() {
-        /* Your code here */
-        return 0.0f;
+        if(sumcredit==0)return 0.0f;
+        else return (float)sum/sumcredit;
     }
     public static void main(String[] args) {
         GPA gpa = new GPA();
