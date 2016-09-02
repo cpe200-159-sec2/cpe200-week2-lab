85e7b0d863f8929e33cf4b1974e6f60832559492
beampanuwit <beampanuwit@hotmail.com>
2016-09-02 16:32:45 +0700

    Yay Week 2

--- lab2_1/src/GPA.java
+++ lab2_1/src/GPA.java
@@ -2,14 +2,18 @@
  * Implement grading system.
  */
 public class GPA {
+    float GPA=0;
+    float Wp=0;
     public void addGrade(int credit, float grade) {
-        /* Your code here */
+        GPA += credit*grade;
+        Wp += credit;
     }
 
 
     public float get() {
-        /* Your code here */
-        return 0.0f;
+        if (Wp==0.0 || GPA==0.0) return 0;
+
+        else  return GPA/Wp;
     }
     public static void main(String[] args) {
         GPA gpa = new GPA();
