c93a44fd642c739f7bf8f28bfe1b3f6eaf1b8abd
nod9n <napon_zupat@hotmail.com>
2016-09-02 16:11:55 +0700

    lab2_2 finish

--- lab2_2/src/ArrayStack.java
+++ lab2_2/src/ArrayStack.java
@@ -20,7 +20,9 @@ public class ArrayStack {
 
     public void push(Object o)
     {
-        /* Your code here */
+        if(storage.length <= size()) throw new RuntimeException("Stack overflow");
+        top++;
+        storage[top] = o;
     }
 
     public Object pop()
@@ -32,8 +34,8 @@ public class ArrayStack {
 
     public int size()
     {
-        /* Your code here */
-        return 0;
+
+        return (top + 1);
     }
 
     public static void main(String[] args) {
