25debe6ed76328ae1b9248170b0709dddd3a561c
patdanai665 <patdanai.duang@gmail.com>
2016-09-02 15:23:52 +0700

    finish lab2_1

--- lab2_1/src/GPA.java
+++ lab2_1/src/GPA.java
@@ -2,14 +2,28 @@
  * Implement grading system.
  */
 public class GPA {
+
+    private int totalCredits;
+    private float totalGrades;
+
+    public GPA(){
+        totalCredits = 0;
+        totalGrades = 0;
+    }
+
     public void addGrade(int credit, float grade) {
-        /* Your code here */
+        totalCredits += credit;
+        totalGrades += grade*credit;
     }
 
 
     public float get() {
-        /* Your code here */
-        return 0.0f;
+        if (totalCredits!=0) {
+            return totalGrades/totalCredits;
+        }
+        else{
+            return 0.0f;
+        }
     }
     public static void main(String[] args) {
         GPA gpa = new GPA();
@@ -17,6 +31,6 @@ public class GPA {
         gpa.addGrade(3, 4);     // Got A for 3 credits class
         gpa.addGrade(3, 3.5f);   // Got B+ for 3 credits class
         gpa.addGrade(2, 2);     // Got C for a 2 credits class
-        System.out.printf("My GPS is %f\n", gpa.get());
+        System.out.printf("My GPA is %f\n", gpa.get());
     }
 }
